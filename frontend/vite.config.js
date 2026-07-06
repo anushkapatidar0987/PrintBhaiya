a1056878d3c533f83f6700732457605569336025
anushkapatidar0987 <anushkapatidar0987@example.com>
2026-07-06 21:40:42 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['better-jokes-peel.loca.lt', 'localhost', 'upccg-2401-4900-1ca3-6812-710c-a25e-169d-8951.free.pinggy.net', 'ehphs-2401-4900-1ca3-6812-710c-a25e-169d-8951.run.pinggy-free.link'],
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
